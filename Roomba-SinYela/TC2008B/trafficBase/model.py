6f0369af0e0d372354c847bb484fee2974a345c5
Andrea Yela <73191386+AnYelg@users.noreply.github.com>
2022-11-24 19:30:38 -0600

    Movement car of all map

--- Roomba-SinYela/TC2008B/trafficBase/model.py
+++ Roomba-SinYela/TC2008B/trafficBase/model.py
@@ -13,7 +13,7 @@ class RandomModel(Model):
     def __init__(self, N):
         self.num_agents = N
         dataDictionary = json.load(open("mapDictionary.json"))
-
+        
         self.traffic_lights = []
 
         with open('2022_base.txt') as baseFile:
@@ -28,23 +28,58 @@ class RandomModel(Model):
                     if col in ["v", "^", ">", "<"]:
                         agent = Road(f"r_{r*self.width+c}", self, dataDictionary[col])
                         self.grid.place_agent(agent, (c, self.height - r - 1))
-
                     elif col in ["S", "s"]:
+                        agent = Road(f"r_{r*self.width+c}", self, dataDictionary[col])
+                        self.grid.place_agent(agent, (c, self.height - r - 1))
                         agent = Traffic_Light(f"tl_{r*self.width+c}", self, False if col == "S" else True, int(dataDictionary[col]))
                         self.grid.place_agent(agent, (c, self.height - r - 1))
                         self.schedule.add(agent)
                         self.traffic_lights.append(agent)
-
                     elif col == "#":
                         agent = Obstacle(f"ob_{r*self.width+c}", self)
                         self.grid.place_agent(agent, (c, self.height - r - 1))
-
                     elif col == "D":
                         agent = Destination(f"d_{r*self.width+c}", self)
                         self.grid.place_agent(agent, (c, self.height - r - 1))
 
+       
+        for i in self.traffic_lights:
+            if i.state:
+                road = self.grid.get_cell_list_contents((i.pos[0]+1,i.pos[1]))
+                for agentR in road:
+                    if agentR.direction == "Left" or agentR.direction == "Right":
+                        roadpos = self.grid.get_cell_list_contents((i.pos[0],i.pos[1]))
+                        for agentRP in roadpos:
+                            if isinstance(agentRP, Road):
+                                agentRP.direction = agentR.direction
+                    else:
+                        road = self.grid.get_cell_list_contents((i.pos[0]-1,i.pos[1]))
+                        for agentR in road:
+                            if agentR.direction == "Left" or agentR.direction == "Right":
+                                roadpos = self.grid.get_cell_list_contents((i.pos[0],i.pos[1]))
+                                for agentRP in roadpos:
+                                    if isinstance(agentRP, Road):
+                                        agentRP.direction = agentR.direction
+            else:
+                road = self.grid.get_cell_list_contents((i.pos[0],i.pos[1]+1))
+                for agentR in road:
+                    if agentR.direction == "Up" or agentR.direction == "Down":
+                        roadpos = self.grid.get_cell_list_contents((i.pos[0],i.pos[1]))
+                        for agentRP in roadpos:
+                            if isinstance(agentRP, Road):
+                                agentRP.direction = agentR.direction
+                    else:
+                        road = self.grid.get_cell_list_contents((i.pos[0],i.pos[1]-1))
+                        for agentR in road:
+                            if agentR.direction == "Up" or agentR.direction == "Down":
+                                roadpos = self.grid.get_cell_list_contents((i.pos[0],i.pos[1]))
+                                for agentRP in roadpos:
+                                    if isinstance(agentRP, Road):
+                                        agentRP.direction = agentR.direction
+                                        
+                
         for i in range(self.num_agents):
-            pos = (1,1)
+            pos = (12,18)
             a = Car(i+1000, self, pos) 
             self.schedule.add(a)
 
